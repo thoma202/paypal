1feba5361dc0de9f077a6f493d86f5442640d04c
Thoma Biguères <tbigueres@202-ecommerce.com>
2016-10-05 11:57:13 +0200

    Verify that patch is correct id_cart

--- controllers/front/pluspatch.php
+++ controllers/front/pluspatch.php
@@ -46,12 +46,15 @@ class PayPalPlusPatchModuleFrontController extends ModuleFrontController
 
     public function postProcess()
     {
-        if(Tools::getValue('id_cart') && Tools::getValue('id_payment'))
+        if(Tools::getValue('id_cart') == $this->context->cart->id)
         {
-            $cart = new Cart(Tools::getValue('id_cart'));
-            $address_delivery = new Address($cart->id_address_delivery);
-            $ppplus = new CallApiPaypalPlus();
-            $result = $ppplus->patch(Tools::getValue('id_payment'),$address_delivery);
+            if(Tools::getValue('id_cart') && Tools::getValue('id_payment'))
+            {
+                $cart = new Cart(Tools::getValue('id_cart'));
+                $address_delivery = new Address($cart->id_address_delivery);
+                $ppplus = new CallApiPaypalPlus();
+                $result = $ppplus->patch(Tools::getValue('id_payment'),$address_delivery);
+            }
         }
     }
 
