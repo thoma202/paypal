eb7bb03a9d2523017ba6959041ac3a8f2d2a6960
Thoma Biguères <tbigueres@202-ecommerce.com>
2016-10-11 18:15:34 +0200

    Upgrade version

--- paypal.php
+++ paypal.php
@@ -98,7 +98,7 @@ class PayPal extends PaymentModule
     {
         $this->name = 'paypal';
         $this->tab = 'payments_gateways';
-        $this->version = '3.11';
+        $this->version = '3.11.1';
         $this->author = 'PrestaShop';
         $this->is_eu_compatible = 1;
 
